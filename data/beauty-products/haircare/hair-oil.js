5e2b54cc7cef8f4052818df8d6f6dfc0400be4a4
dailyglamstore <dmahajan152@gmail.com>
2026-04-19 22:02:33 +0530

    Update hair-oil.js

--- data/beauty-products/haircare/hair-oil.js
+++ data/beauty-products/haircare/hair-oil.js
@@ -1,4 +1,21 @@
 (function () {
   // Haircare oil products. Upload future images to: images/beauty-images/haircare/hair-oil/
-  window.BEAUTY_HAIRCARE_HAIR_OIL_PRODUCTS = [];
+  window.BEAUTY_HAIRCARE_HAIR_OIL_PRODUCTS = [
+{
+  id: "lotus-red-onion-hair-fall-control-hair-oil",
+  name: "Red Onion Hair-Fall Control Hair Oil",
+  brand: "Lotus Botanicals",
+  image: "images/beauty-images/haircare/hair-oil/lotus-red-onion-hair-fall-control-hair-oil.JPG",
+  url: "https://www.lotusbotanicals.com/products/red-onion-hair-fall-control-hair-oil?ref=DGS-Deepak",
+  priceText: "₹595 + 20% OFF with our code",
+  details: [
+    "Helps reduce hair fall",
+    "Red Onion supports stronger roots",
+    "Nourishes scalp and hair lengths",
+    "Good for pre-wash oiling",
+    "Supports softer, healthier-looking hair",
+    "Best for weak & thinning hair"
+  ]
+}
+];
 })();
