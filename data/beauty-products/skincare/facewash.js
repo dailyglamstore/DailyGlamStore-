093af79aa26fa979b8d43a15de42a9e3988e93ce
dailyglamstore <dmahajan152@gmail.com>
2026-04-19 22:00:24 +0530

    Update facewash.js

--- data/beauty-products/skincare/facewash.js
+++ data/beauty-products/skincare/facewash.js
@@ -2,17 +2,20 @@
   // Skincare face wash products. Upload future images to: images/beauty-images/skincare/facewash/
   window.BEAUTY_SKINCARE_FACEWASH_PRODUCTS = [
     {
-      id: "plum-face-wash-recommendation",
-      name: "Face Wash Recommendation",
-      brand: "Plum",
-      image: "images/beauty-images/top-picks/lotus-vitamin-c-skin-brightening-day-creme.JPG",
-      url: "https://plumgoodness.com/",
-      priceText: "₹685 + 20% OFF with our code",
-      details: [
-        "Use a gentle cleanser for daily routine",
-        "Pick based on skin type and concern",
-        "Avoid over-cleansing to protect barrier"
-      ]
-    }
+  id: "lotus-ubtan-de-tan-radiance-face-wash",
+  name: "Ubtan De-Tan Radiance Face Wash",
+  brand: "Lotus Botanicals",
+  image: "images/beauty-images/skincare/facewash/lotus-ubtan-de-tan-radiance-face-wash.JPG",
+  url: "https://www.lotusbotanicals.com/products/copy-of-ubtan-de-tan-radiance-face-wash?ref=DGS-Deepak",
+  priceText: "₹295 + 20% OFF with our code",
+  details: [
+    "Helps remove tan & dullness",
+    "Ubtan glow care for fresh skin",
+    "Gently cleanses daily dirt",
+    "Supports brighter skin tone",
+    "Good for regular face wash use",
+    "Best for dull & sun-exposed skin"
+  ]
+}
   ];
 })();
