cac58cc9d3e4c85751e0e5dd347f77e28f5e125e
dailyglamstore <dmahajan152@gmail.com>
2026-04-24 16:29:40 +0530

    Update hair-serum.js

--- data/beauty-products/haircare/hair-serum.js
+++ data/beauty-products/haircare/hair-serum.js
@@ -2,16 +2,6 @@
   // Haircare serum products. Upload future images to: images/beauty-images/haircare/hair-serum/
   window.BEAUTY_HAIRCARE_HAIR_SERUM_PRODUCTS = [
     {
-      id: "lotus-hair-serum-recommendation",
-      name: "Hair Serum Recommendation",
-      brand: "Lotus Botanicals",
-      image: "images/beauty-images/top-picks/lotus-vitamin-c-skin-brightening-day-creme.JPG",
-      url: "https://www.lotusbotanicals.com/",
-      priceText: "₹685 + 20% OFF with our code",
-      details: [
-        "Apply on lengths to manage frizz",
-        "Use small quantity to avoid heaviness",
-        "Pairs well with conditioner for smoother hair"
       ]
     }
   ];
