20640df00ab09ecdc1bfd23fbf4971c61bb9d8ad
dailyglamstore <dmahajan152@gmail.com>
2026-04-24 17:18:11 +0530

    Update conditioner.js

--- data/beauty-products/haircare/conditioner.js
+++ data/beauty-products/haircare/conditioner.js
@@ -1,4 +1,21 @@
 (function () {
   // Haircare conditioner products. Upload future images to: images/beauty-images/haircare/conditioner/
-  window.BEAUTY_HAIRCARE_CONDITIONER_PRODUCTS = [];
+  window.BEAUTY_HAIRCARE_CONDITIONER_PRODUCTS = [
+{
+  id: "lotus-red-onion-conditioner",
+  name: "Red Onion Nutritive Hair Conditioner",
+  brand: "Lotus Botanicals",
+  image: "images/beauty-images/haircare/conditioner/lotus-red-onion-conditioner.JPG",
+  url: "https://www.lotusbotanicals.com/products/red-onion-nutritive-hair-conditioner?ref=DGS-Deepak",
+  priceText: "₹395 + 20% OFF with our code",
+  details: [
+    "Reduces hair fall & strengthens roots",
+    "Red Onion extract boosts growth & scalp health",
+    "Smoothens frizz and detangles hair",
+    "Use after shampoo, leave 2-3 mins",
+    "Makes hair soft, shiny & manageable",
+    "Best for dry, frizzy & weak hair"
+  ]
+}
+];
 })();
