10f35d5b96926073023e3eb16fc6bbb58be6e666
dailyglamstore <dmahajan152@gmail.com>
2026-04-24 17:19:55 +0530

    Update moisturiser.js

--- data/beauty-products/skincare/moisturiser.js
+++ data/beauty-products/skincare/moisturiser.js
@@ -1,4 +1,21 @@
 (function () {
   // Skincare moisturiser products. Upload future images to: images/beauty-images/skincare/moisturiser/
-  window.BEAUTY_SKINCARE_MOISTURISER_PRODUCTS = [];
+  window.BEAUTY_SKINCARE_MOISTURISER_PRODUCTS = [
+    {
+  id: "lotus-vitamin-c-moisturiser",
+  name: "Vitamin C Skin Brightening Face Moisturiser",
+  brand: "Lotus Botanicals",
+  image: "images/beauty-images/skincare/moisturiser/lotus-vitamin-c-moisturiser.JPG",
+  url: "https://www.lotusbotanicals.com/products/vitamin-c-skin-brightening-face-moisturiser?ref=DGS-Deepak",
+  priceText: "₹415 + 20% OFF with our code",
+  details: [
+    "Brightens skin & boosts natural glow",
+    "Vitamin C reduces dark spots",
+    "Hydrates without oily feel",
+    "Use daily on clean face & neck",
+    "Gives soft, radiant, even skin",
+    "Best for dull & uneven skin"
+  ]
+}
+];
 })();
