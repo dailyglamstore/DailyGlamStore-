95f84d9dac8ad9d119a74fe6f7bf8b75503b7433
dailyglamstore <dmahajan152@gmail.com>
2026-04-24 18:05:04 +0530

    Update serum.js

--- data/beauty-products/skincare/serum.js
+++ data/beauty-products/skincare/serum.js
@@ -1,4 +1,21 @@
 (function () {
   // Skincare serum products. Upload future images to: images/beauty-images/skincare/serum/
-  window.BEAUTY_SKINCARE_SERUM_PRODUCTS = [];
+  window.BEAUTY_SKINCARE_SERUM_PRODUCTS = [
+    {
+      id: "lotus-vitamin-c-serum",
+      name: "Vitamin C Skin Brightening Face Serum",
+      brand: "Lotus Botanicals",
+      image: "images/beauty-images/skincare/serum/lotus-vitamin-c-serum.JPG",
+      url: "https://www.lotusbotanicals.com/products/vitamin-c-skin-brightening-face-serum?ref=DGS-Deepak",
+      priceText: "₹845 + 20% OFF with our code",
+      details: [
+        "Brightens dull skin & boosts glow",
+        "Vitamin C fades dark spots",
+        "Lightweight, fast-absorbing formula",
+        "Apply 2-3 drops on clean face daily",
+        "Gives radiant, even-toned skin",
+        "Best for dull & pigmented skin"
+      ]
+    }
+  ];
 })();
