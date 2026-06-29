652a9ac930d5d426ca83917f38baa8bb37ffa510
dailyglamstore <dmahajan152@gmail.com>
2026-06-29 20:58:26 +0530

    Create publisher.js

---
+++ publisher.js
@@ -0,0 +1,9 @@
+window.DGS_PUBLISHER = {
+  websiteHealth: "Excellent",
+  lastScan: "29 Jun 2026",
+
+  articles: {
+    skincare: window.SKINCARE_ARTICLES || {},
+    haircare: window.HAIRCARE_ARTICLES || {}
+  }
+};
