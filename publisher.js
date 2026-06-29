43d8c744b6c701490975e26226c6c548503c13e3
dailyglamstore <dmahajan152@gmail.com>
2026-06-29 21:46:06 +0530

    Delete publisher.js

--- publisher.js
+++
@@ -1,9 +0,0 @@
-window.DGS_PUBLISHER = {
-  websiteHealth: "Excellent",
-  lastScan: "29 Jun 2026",
-
-  articles: {
-    skincare: window.SKINCARE_ARTICLES || {},
-    haircare: window.HAIRCARE_ARTICLES || {}
-  }
-};
