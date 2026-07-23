731a11c6ddd354469b4856743f7bd0487a552fd0
dailyglamstore <dmahajan152@gmail.com>
2026-07-23 22:10:46 +0530

    Create lorem-ipsum.js

---
+++ skincare-articles/lorem-ipsum.js
@@ -0,0 +1,13 @@
+// File: /skincare-articles/lorem-ipsum.js
+window.currentArticleData = {
+  title: "Lorem Ipsum Test Article",
+  date: "July 2026",
+  category: "Skincare",
+  content: `
+    <h2>Test Heading One</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    
+    <h2>Test Heading Two</h2>
+    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  `
+};
