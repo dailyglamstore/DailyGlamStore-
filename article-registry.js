14cfa8cd738c791e082c4c840ec0c2fb7ea63e41
dailyglamstore <dmahajan152@gmail.com>
2026-07-25 17:45:30 +0530

    Create article-registry.js

---
+++ article-registry.js
@@ -0,0 +1,11 @@
+window.ARTICLE_REGISTRY = {
+  skincare: [
+    "skincare-routine",
+    "how-to-choose-face-wash",
+    "lotus-vs-bodywise-sunscreen"
+  ],
+
+  haircare: [
+    "haircare-routine"
+  ]
+};
