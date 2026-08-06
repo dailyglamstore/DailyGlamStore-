5562f91d6788ef9e0f2a43551ca9a4b05dd94f03
dailyglamstore <dmahajan152@gmail.com>
2026-08-06 15:40:29 +0530

    Update facewash.js

--- data/beauty-products/skincare/facewash.js
+++ data/beauty-products/skincare/facewash.js
@@ -5,7 +5,7 @@
       name: "Ubtan De-Tan Radiance Face Wash",
       brand: "Lotus Botanicals",
       image: "images/beauty-images/skincare/facewash/lotus-ubtan-de-tan-radiance-face-wash.JPG",
-      amazonUrl: "https://www.amazon.in/dp/YOUR_AMAZON_ASIN?tag=your-affiliate-id", // Primary Button
+      amazonUrl: " ", // Primary Button
       url: "https://www.lotusbotanicals.com/products/copy-of-ubtan-de-tan-radiance-face-wash?ref=DGS-Deepak", // Secondary Button
       priceText: "₹295 | Offers Available",
       details: [
