06c856870a5fd8877d5e204cc1f95a4c81e106eb
dailyglamstore <dmahajan152@gmail.com>
2026-08-12 01:25:18 +1000

    Update lip-care.js

--- data/beauty-products/makeup/lip-care.js
+++ data/beauty-products/makeup/lip-care.js
@@ -5,9 +5,9 @@
       name: "Ceramide Tinted Lip Balm with SPF 50 PA++++",
       brand: "WishCare",
       images: [
-        "/images/beauty-images/makeup/lip-care/wishcare-ceramide-tinted-lip-balm-spf50-tube.JPG",
-        "/images/beauty-images/makeup/lip-care/wishcare-ceramide-tinted-lip-balm-spf50-before-after.JPG",
-        "/images/beauty-images/makeup/lip-care/wishcare-ceramide-tinted-lip-balm-spf50-benefits.JPG"
+        "images/beauty-images/makeup/lip-care/wishcare-ceramide-tinted-lip-balm-spf50-tube.JPG",
+        "images/beauty-images/makeup/lip-care/wishcare-ceramide-tinted-lip-balm-spf50-before-after.JPG",
+        "images/beauty-images/makeup/lip-care/wishcare-ceramide-tinted-lip-balm-spf50-benefits.JPG"
       ],
       amazonUrl: "https://link.amazon/B0izgYZO8",
       url: "https://www.mywishcare.com/products/tinted-lip-balm-with-spf50",
