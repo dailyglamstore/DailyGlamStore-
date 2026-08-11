c0228ed22abdc9dc47d56b04813d9ab0fc76ddf4
dailyglamstore <dmahajan152@gmail.com>
2026-08-12 04:43:27 +1000

    Create lip-care.js

---
+++ data/beauty-products/makeup/lip-care.js
@@ -0,0 +1,25 @@
+(function () {
+  window.BEAUTY_COSMETICS_LIP_PRODUCTS = [
+    {
+      id: "wishcare-ceramide-tinted-lip-balm-spf50",
+      name: "Ceramide Tinted Lip Balm with SPF 50 PA++++",
+      brand: "WishCare",
+      images: [
+        "images/beauty-images/makeup/lip-care/wishcare-lip-1.JPG",
+        "images/beauty-images/makeup/lip-care/wishcare-lip-2.JPG",
+        "images/beauty-images/makeup/lip-care/wishcare-lip-3.JPG"
+      ],
+      amazonUrl: "https://link.amazon/B0izgYZO8",
+      url: "https://www.mywishcare.com/products/tinted-lip-balm-with-spf50",
+      priceText: "₹295 | Offers Available",
+      details: [
+        "SPF 50+ PA++++ protects against sun-induced lip darkening",
+        "Ceramides repair & strengthen dry, damaged lip barriers",
+        "Kojic Acid & Niacinamide fade lip hyperpigmentation",
+        "Gives a natural, non-sticky pink tint in one swipe",
+        "Shea & Cocoa Butters deliver long-lasting hydration",
+        "Ideal for daily sun protection and lip brightening"
+      ]
+    }
+  ];
+})();
