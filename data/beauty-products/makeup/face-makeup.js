62f04f6a724b3b1bea02d0dd830acd89e8a015c1
dailyglamstore <dmahajan152@gmail.com>
2026-08-11 22:20:13 +1000

    Update face-makeup.js

--- data/beauty-products/makeup/face-makeup.js
+++ data/beauty-products/makeup/face-makeup.js
@@ -1,3 +1,25 @@
 (function () {
-  window.BEAUTY_COSMETICS_FACE_PRODUCTS = [];
+  window.BEAUTY_COSMETICS_FACE_PRODUCTS = [
+    {
+      id: "sotrue-3-in-1-illuminating-strobe-cream-pink",
+      name: "3-in-1 Illuminating Strobe Cream (Pink)",
+      brand: "Sotrue",
+      images: [
+        "images/beauty-images/makeup/face-makeup/sotrue-3-in-1-illuminating-strobe-cream-pink-tube.JPG",
+        "images/beauty-images/makeup/face-makeup/sotrue-3-in-1-illuminating-strobe-cream-pink-before-after.JPG",
+        "images/beauty-images/makeup/face-makeup/sotrue-3-in-1-illuminating-strobe-cream-pink-usage.JPG"
+      ],
+      amazonUrl: "https://link.amazon/B0dHL3heh",
+      url: "https://www.sotrue.in/products/strobe-cream",
+      priceText: "MRP ₹349 | Offers Available",
+      details: [
+        "3-in-1 primer, highlighter, and hydrating moisturizer",
+        "Hyaluronic Acid & Niacinamide give non-sticky glow",
+        "Superfine pearl particles create natural, dewy shine",
+        "Use under foundation, alone, or on cheekbones",
+        "Lightweight formula that won't clog facial pores",
+        "Best for fair to medium tones wanting instant glow"
+      ]
+    }
+  ];
 })();
