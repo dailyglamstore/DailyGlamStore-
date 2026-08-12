3c9c8516d5daf5af31cb4442f222363ac89c25d9
dailyglamstore <dmahajan152@gmail.com>
2026-08-13 01:17:27 +1000

    Update hair-serum.js

--- data/beauty-products/haircare/hair-serum.js
+++ data/beauty-products/haircare/hair-serum.js
@@ -1,8 +1,4 @@
 (function () {
   // Haircare serum products. Upload future images to: images/beauty-images/haircare/hair-serum/
-  window.BEAUTY_HAIRCARE_HAIR_SERUM_PRODUCTS = [
-    {
-      ]
-    }
-  ];
+  window.BEAUTY_HAIRCARE_HAIR_SERUM_PRODUCTS = [];
 })();
