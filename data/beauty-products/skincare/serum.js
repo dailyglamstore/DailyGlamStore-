99d69aae6278a8dca222c880f2dc35b498574f4a
dailyglamstore <dmahajan152@gmail.com>
2026-09-03 22:00:26 +1000

    Update serum.js

--- data/beauty-products/skincare/serum.js
+++ data/beauty-products/skincare/serum.js
@@ -6,7 +6,7 @@
       name: "Vitamin C Skin Brightening Face Serum",
       brand: "Lotus Botanicals",
       image: "images/beauty-images/skincare/serum/lotus-vitamin-c-serum.JPG",
-      url: "https://linksredirect.com/?cid=315768&source=linkkit&url=https%3A%2F%2Fplumgoodness.com%2F",
+      url: "https://www.lotusbotanicals.com/products/vitamin-c-skin-brightening-face-serum?ref=DGS-Deepak",
       priceText: "₹845 | Offers Available",
       details: [
         "Brightens dull skin & boosts glow",
