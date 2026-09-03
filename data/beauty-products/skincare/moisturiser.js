ca6b595c2cb337aeb637e5fb17e1cc2f3a2037a8
dailyglamstore <dmahajan152@gmail.com>
2026-09-03 22:04:30 +1000

    Update moisturiser.js

--- data/beauty-products/skincare/moisturiser.js
+++ data/beauty-products/skincare/moisturiser.js
@@ -6,7 +6,7 @@
   name: "Vitamin C Skin Brightening Face Moisturiser",
   brand: "Lotus Botanicals",
   image: "images/beauty-images/skincare/moisturiser/lotus-vitamin-c-moisturiser.JPG",
-  url: "https://plumgoodness.com/products/thinkderma-2-niacinamide-rice-water-clear-moisture-gel-cream-all-day-non-greasy-moisturizer-fades-blemishes-brightens-skin-3-matmarine-for-oil-control-lightweight-non-sticky-100-vegan",
+  url: "https://www.lotusbotanicals.com/products/vitamin-c-skin-brightening-face-moisturiser?ref=DGS-Deepak",
   priceText: "₹595 | Offers Available",
   details: [
     "Brightens skin & boosts natural glow",
